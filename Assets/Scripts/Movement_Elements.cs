18a6609ab416ade4ee1558b5b8d873bc89bb844d
Jose Luis Jimenez <ijos@ijos.es>
2015-05-14 22:07:28 +0200

    Menu working

--- Assets/Scripts/Movement_Elements.cs
+++ Assets/Scripts/Movement_Elements.cs
@@ -2,14 +2,36 @@
 using System.Collections;
 
 public class Movement_Elements : MonoBehaviour {
-
+	float verticalspeed = 5.0f;
+	float control_var = 0.0f;
+	Rigidbody rectangle;
 	// Use this for initialization
 	void Start () {
+		rectangle = GetComponent<Rigidbody>();
+		control_var = 0.0f;
+	}
 	
+	void MoveElements() {
+		if (control_var >= 0.0f) {
+			MoveUp ();
+		} else if (control_var >= 10.0f) {
+			MoveDown ();
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void MoveUp(){
+		transform.Translate (Vector3.up * verticalspeed * Time.deltaTime);
+		control_var++;
 	}
+	void MoveDown() {
+		transform.Translate (Vector3.down * verticalspeed * Time.deltaTime);
+		control_var--;
+	}
+	
+	void Update () {
+		
+		MoveElements ();
+	}
+	
 }
---
+++ Assets/Scripts/change_scene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class change_scene : MonoBehaviour {
+
+	public void changeToScene(int scene){
+		Application.LoadLevel (scene);
+	}
+
+}
