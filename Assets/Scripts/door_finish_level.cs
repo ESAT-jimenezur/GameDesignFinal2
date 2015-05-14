08030a5602e89f65262e491d8a08ee73977bfc67
Jose Luis Jimenez <ijos@ijos.es>
2015-05-14 22:23:29 +0200

    More changes

---
+++ Assets/Scripts/door_finish_level.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door_finish_level : MonoBehaviour {
+	
+	bool player_blue_inside = false;
+	bool player_red_inside = false;
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "Player 1") {
+			player_blue_inside = true;
+		}
+		if (other.gameObject.name == "Player 2") {
+			player_red_inside = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.name == "Player 1") {
+			player_blue_inside = false;
+		}
+		if (other.gameObject.name == "Player 2") {
+			player_red_inside = false;
+		}
+	}
+
+
+	void Update(){
+		if(player_blue_inside && player_red_inside){
+			Application.LoadLevel(0);
+		}
+	}
+
+}
