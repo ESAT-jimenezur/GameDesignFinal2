77ec3b951f04c832bf4ace6c90863d2818ba38c4
Jose Luis Jimenez <ijos@ijos.es>
2015-05-14 19:44:47 +0200

    Better jump movement

---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_movement : MonoBehaviour {
+
+	float speed = 5.0f;
+	float jump_speed = 5.0f;
+	public int player_number = 0;
+
+	Rigidbody rb;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		//lineRenderer.SetPosition(0, transform.position);
+		//lineRenderer.SetPosition(1, Input.mousePosition);
+		
+		playerMovement();
+		
+	}
+	
+	
+	void playerMovement(){
+		
+		if (player_number == 1) {
+			if (Input.GetKey(KeyCode.D)){
+				moveRight();
+			}else if(Input.GetKey(KeyCode.A)){
+				moveLeft();
+			}
+			if (Input.GetKeyDown ("space")){
+				rb.velocity = new Vector3(0, jump_speed, 0);
+			} 
+		} else if (player_number == 2) {
+			if (Input.GetKey(KeyCode.RightArrow)){
+				moveRight();
+			}else if(Input.GetKey(KeyCode.LeftArrow)){
+				moveLeft();
+			}
+		}
+		
+	}
+	
+	void moveRight(){
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
+	}
+	
+	void moveLeft(){
+		transform.Translate (Vector3.left * speed * Time.deltaTime);
+	}
+}
